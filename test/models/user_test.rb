36960f9b7e99ba2632722e360bdaf1b924041655
Alice Amy Dee <amyalicedee@gmail.com>
2016-02-22 21:28:57 +0000

    Make basic user model, include secure passwords

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,65 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com", password:"foobar", password_confirmation: "foobar")
+  end
+
+  test "user_is_valid" do
+    assert @user.valid?
+  end
+
+  test "user_has_name_present" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should only accept walid emails" do
+    valid_addresses = %w[user@example.com USER@user.co.uk]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid emails" do
+    invalid_addresses = %w[user@example,com user_at_eg.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email address should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
+require "minitest/autorun"
+require "minitest/pride"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
